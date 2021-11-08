8de19dcce807d076ab54a4d6605b95a570731551
htchepannou <htchepannou@expedia.com>
2021-11-08 12:36:07 -0500

    fix lint issues

--- src/main/kotlin/com/wutsi/application/home/endpoint/home/HomeScreen.kt
+++ src/main/kotlin/com/wutsi/application/home/endpoint/home/HomeScreen.kt
@@ -69,7 +69,6 @@ class HomeScreen(
                 crossAxisAlignment = CrossAxisAlignment.center,
                 mainAxisAlignment = spaceEvenly
             ),
-
-            ).toWidget()
+        ).toWidget()
     }
 }
