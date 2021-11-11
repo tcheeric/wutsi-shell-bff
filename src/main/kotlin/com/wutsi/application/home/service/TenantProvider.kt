5eb72f7c319aebe84f89027620e90e37853751bc
htchepannou <htchepannou@expedia.com>
2021-11-11 14:54:29 -0500

    fix resolution of tenant-id

--- src/main/kotlin/com/wutsi/application/home/service/TenantProvider.kt
+++ src/main/kotlin/com/wutsi/application/home/service/TenantProvider.kt
@@ -1,5 +1,6 @@
 package com.wutsi.application.home.service
 
+import com.wutsi.platform.core.tracing.TracingContext
 import com.wutsi.platform.tenant.WutsiTenantApi
 import com.wutsi.platform.tenant.dto.MobileCarrier
 import com.wutsi.platform.tenant.dto.Tenant
@@ -7,14 +8,18 @@ import org.springframework.stereotype.Service
 
 @Service
 class TenantProvider(
-    private val tenantApi: WutsiTenantApi
+    private val tenantApi: WutsiTenantApi,
+    private val tracingContext: TracingContext,
 ) {
     fun get(): Tenant =
-        tenantApi.getTenant(1).tenant
+        tenantApi.getTenant(tenantId()).tenant
 
     fun mobileCarriers(tenant: Tenant) =
         tenant.mobileCarriers.filter { it.countries.any { it in tenant.countries } }
 
     fun logo(carrier: MobileCarrier): String? =
         carrier.logos.find { it.type == "PICTORIAL" }?.url
+
+    private fun tenantId(): Long =
+        tracingContext.tenantId()?.toLong() ?: 1
 }
