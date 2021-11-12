8af07b3ba7f5f26947f3898c05c8aaaf2cfe6df3
htchepannou <htchepannou@expedia.com>
2021-11-12 18:01:34 -0500

    do not crash when resolving the balance

--- src/main/kotlin/com/wutsi/application/home/service/PaymentService.kt
+++ src/main/kotlin/com/wutsi/application/home/service/PaymentService.kt
@@ -4,6 +4,7 @@ import com.wutsi.platform.payment.WutsiPaymentApi
 import com.wutsi.platform.payment.core.Money
 import com.wutsi.platform.tenant.dto.Tenant
 import feign.FeignException
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,6 +12,10 @@ class PaymentService(
     private val paymentApi: WutsiPaymentApi,
     private val userProvider: UserProvider,
 ) {
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(PaymentService::class.java)
+    }
+
     fun getBalance(tenant: Tenant): Money {
         try {
             val userId = userProvider.id()
@@ -20,6 +25,9 @@ class PaymentService(
                 currency = account.currency
             )
         } catch (ex: FeignException.NotFound) {
+            return Money(currency = tenant.currency)
+        } catch (ex: Throwable) {
+            LOGGER.error("Unexpected error while resolving the balance", ex)
             return Money(currency = tenant.currency)
         }
     }
