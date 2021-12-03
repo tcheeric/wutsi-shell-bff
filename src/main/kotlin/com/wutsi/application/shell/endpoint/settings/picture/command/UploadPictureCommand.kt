2c9c86dadea027f0cedd44e92eff5df0c5ce9ebf
wutsi <herve.tchepannou@wutsi.com>
2021-12-03 14:43:07 -0500

    add picture page

---
+++ src/main/kotlin/com/wutsi/application/shell/endpoint/settings/picture/command/UploadPictureCommand.kt
@@ -0,0 +1,44 @@
+package com.wutsi.application.shell.endpoint.settings.picture.command
+
+import com.wutsi.application.shell.endpoint.AbstractCommand
+import com.wutsi.application.shell.service.UserProvider
+import com.wutsi.platform.account.WutsiAccountApi
+import com.wutsi.platform.account.dto.UpdateAccountAttributeRequest
+import com.wutsi.platform.core.logging.KVLogger
+import com.wutsi.platform.core.storage.StorageService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.util.UUID
+
+@RestController
+@RequestMapping("/commands/upload-picture")
+class UploadPictureCommand(
+    private val accountApi: WutsiAccountApi,
+    private val storageService: StorageService,
+    private val userProvider: UserProvider,
+    private val logger: KVLogger
+) : AbstractCommand() {
+    @PostMapping
+    fun index(@RequestParam file: MultipartFile) {
+        logger.add("file_name", file.originalFilename)
+        logger.add("file_content_type", file.contentType)
+
+        // Upload file
+        val userId = userProvider.id()
+        val path = "user/$userId/picture/${UUID.randomUUID()}-${file.originalFilename}"
+        val url = storageService.store(path, file.inputStream, file.contentType)
+        logger.add("picture_url", url)
+
+        // Update user profile
+        accountApi.updateAccountAttribute(
+            id = userId,
+            name = "picture-url",
+            request = UpdateAccountAttributeRequest(
+                value = url.toString()
+            )
+        )
+    }
+}
