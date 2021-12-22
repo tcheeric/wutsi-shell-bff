a0b7a2d3dc50d17239c4472842c2cbddadda6cfc
wutsi <herve.tchepannou@wutsi.com>
2021-12-22 09:26:39 -0500

    update picture page

--- src/main/kotlin/com/wutsi/application/shell/endpoint/settings/picture/screen/SettingsPictureScreen.kt
+++ src/main/kotlin/com/wutsi/application/shell/endpoint/settings/picture/screen/SettingsPictureScreen.kt
@@ -4,13 +4,24 @@ import com.wutsi.application.shell.endpoint.AbstractQuery
 import com.wutsi.application.shell.endpoint.Page
 import com.wutsi.application.shell.endpoint.Theme
 import com.wutsi.application.shell.service.URLBuilder
+import com.wutsi.application.shell.service.UserProvider
 import com.wutsi.flutter.sdui.Action
 import com.wutsi.flutter.sdui.AppBar
-import com.wutsi.flutter.sdui.Camera
+import com.wutsi.flutter.sdui.Column
+import com.wutsi.flutter.sdui.Container
+import com.wutsi.flutter.sdui.Divider
+import com.wutsi.flutter.sdui.Icon
+import com.wutsi.flutter.sdui.Image
+import com.wutsi.flutter.sdui.Input
+import com.wutsi.flutter.sdui.Row
 import com.wutsi.flutter.sdui.Screen
 import com.wutsi.flutter.sdui.Widget
-import com.wutsi.flutter.sdui.enums.ActionType.Route
-import com.wutsi.flutter.sdui.enums.CameraLensDirection
+import com.wutsi.flutter.sdui.enums.ActionType
+import com.wutsi.flutter.sdui.enums.Alignment
+import com.wutsi.flutter.sdui.enums.CrossAxisAlignment
+import com.wutsi.flutter.sdui.enums.ImageSource
+import com.wutsi.flutter.sdui.enums.InputType
+import com.wutsi.flutter.sdui.enums.MainAxisAlignment
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -19,9 +30,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/settings/picture")
 class SettingsPictureScreen(
     private val urlBuilder: URLBuilder,
+    private val userProvider: UserProvider
 ) : AbstractQuery() {
     @PostMapping
     fun index(): Widget {
+        val me = userProvider.get()
+        val pictureUrl = me.pictureUrl ?: ""
         return Screen(
             id = Page.SETTINGS_PICTURE,
             backgroundColor = Theme.WHITE_COLOR,
@@ -31,15 +45,68 @@ class SettingsPictureScreen(
                 foregroundColor = Theme.BLACK_COLOR,
                 title = getText("page.settings.picture.app-bar.title"),
             ),
-            child = Camera(
-                uploadUrl = urlBuilder.build("commands/upload-picture"),
-                name = "file",
-                lensDirection = CameraLensDirection.front,
-                action = Action(
-                    type = Route,
-                    url = "route:/.."
-                ),
-            ),
+            child = Column(
+                crossAxisAlignment = CrossAxisAlignment.center,
+                children = listOf(
+                    Row(
+                        mainAxisAlignment = MainAxisAlignment.center,
+                        children = listOf(
+                            Container(
+                                padding = 10.0,
+                                alignment = Alignment.Center,
+                                child = Image(
+                                    url = pictureUrl,
+                                    width = 256.0,
+                                    height = 256.0
+                                )
+                            )
+                        ),
+                    ),
+                    Divider(color = Theme.DIVIDER_COLOR),
+                    Row(
+                        mainAxisAlignment = MainAxisAlignment.center,
+                        children = listOf(
+                            Icon(
+                                code = Theme.ICON_CAMERA,
+                                size = 24.0
+                            ),
+                            Input(
+                                name = "file",
+                                uploadUrl = urlBuilder.build("/command/upload-picture"),
+                                type = InputType.Image,
+                                imageSource = ImageSource.Camera,
+                                caption = "Camera",
+                                action = Action(
+                                    type = ActionType.Route,
+                                    url = "route:/.."
+                                ),
+                            )
+                        ),
+                    ),
+                    Divider(color = Theme.DIVIDER_COLOR),
+                    Row(
+                        mainAxisAlignment = MainAxisAlignment.center,
+                        children = listOf(
+                            Icon(
+                                code = Theme.ICON_FOLDER,
+                                size = 24.0
+                            ),
+                            Input(
+                                name = "file",
+                                uploadUrl = urlBuilder.build("/command/upload-picture"),
+                                type = InputType.Image,
+                                imageSource = ImageSource.Gallery,
+                                caption = "Gallery",
+                                action = Action(
+                                    type = ActionType.Route,
+                                    url = "route:/.."
+                                )
+                            )
+                        ),
+                    ),
+                    Divider(color = Theme.DIVIDER_COLOR),
+                )
+            )
         ).toWidget()
     }
 }
