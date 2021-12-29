2099c4444727927c1dbccda9d201745df50b8eab
wutsi <herve.tchepannou@wutsi.com>
2021-12-28 19:49:09 -0500

    set submit url

--- src/main/kotlin/com/wutsi/application/shell/endpoint/pay/screen/ProcessScreen.kt
+++ src/main/kotlin/com/wutsi/application/shell/endpoint/pay/screen/ProcessScreen.kt
@@ -2,46 +2,43 @@ package com.wutsi.application.shell.endpoint.pay.screen
 
 import com.wutsi.application.shell.endpoint.AbstractQuery
 import com.wutsi.application.shell.endpoint.Theme
+import com.wutsi.application.shell.endpoint.pay.dto.ScanRequest
 import com.wutsi.application.shell.service.URLBuilder
 import com.wutsi.flutter.sdui.AppBar
 import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Container
-import com.wutsi.flutter.sdui.Flexible
-import com.wutsi.flutter.sdui.QrView
+import com.wutsi.flutter.sdui.Icon
 import com.wutsi.flutter.sdui.Screen
-import com.wutsi.flutter.sdui.Text
 import com.wutsi.flutter.sdui.Widget
 import com.wutsi.flutter.sdui.enums.Alignment
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/pay/scan")
-class ScanScreen(
+@RequestMapping("/pay/process")
+class ProcessScreen(
     private val urlBuilder: URLBuilder
 ) : AbstractQuery() {
     @PostMapping
-    fun index(): Widget {
+    fun index(@RequestBody request: ScanRequest): Widget {
+        println(">>>> ${request.format}")
+        println(">>>> ${request.code}")
         return Screen(
-            id = "page.pay.scan",
+            id = "page.pay.process",
             appBar = AppBar(
                 elevation = 0.0,
                 backgroundColor = Theme.WHITE_COLOR,
                 foregroundColor = Theme.BLACK_COLOR,
-                title = "Scan"
+                title = "Pay"
             ),
             child = Column(
                 children = listOf(
                     Container(
                         padding = 10.0,
                         alignment = Alignment.Center,
-                        child = Text("Scan the QR Code")
-                    ),
-                    Flexible(
-                        child = QrView(
-                            submitUrl = urlBuilder.build("/pay/process")
-                        )
+                        child = Icon(code = Theme.ICON_CHECK, color = Theme.SUCCESS_COLOR, size = 64.0)
                     ),
                 ),
             )
