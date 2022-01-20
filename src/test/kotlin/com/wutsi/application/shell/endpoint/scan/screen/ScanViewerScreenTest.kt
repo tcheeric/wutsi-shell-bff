15f2b497b7bd842c5199f38af615cccca575c13b
wutsi <herve.tchepannou@wutsi.com>
2022-01-20 13:08:54 -0500

    revert

--- src/test/kotlin/com/wutsi/application/shell/endpoint/scan/screen/ScanViewerScreenTest.kt
+++ src/test/kotlin/com/wutsi/application/shell/endpoint/scan/screen/ScanViewerScreenTest.kt
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.web.server.LocalServerPort
-import kotlin.test.Ignore
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class ScanViewerScreenTest : AbstractEndpointTest() {
@@ -84,7 +83,6 @@ internal class ScanViewerScreenTest : AbstractEndpointTest() {
     }
 
     @Test
-    @Ignore
     fun transactionApproval() {
         // GIVEN
         val entity = Entity("transaction-approval", "xxxx")
