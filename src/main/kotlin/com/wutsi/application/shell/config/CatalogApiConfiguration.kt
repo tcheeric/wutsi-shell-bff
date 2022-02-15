2feebddad5180d27260d30ca936ecd5f7a082fe4
wutsi <herve.tchepannou@wutsi.com>
2022-02-15 18:04:03 -0500

    update dependecies

--- src/main/kotlin/com/wutsi/application/shell/config/CatalogApiConfiguration.kt
+++ src/main/kotlin/com/wutsi/application/shell/config/CatalogApiConfiguration.kt
@@ -1,18 +1,19 @@
 package com.wutsi.application.shell.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.wutsi.application.shared.service.FeignAcceptLanguageInterceptor
 import com.wutsi.platform.catalog.Environment.PRODUCTION
 import com.wutsi.platform.catalog.Environment.SANDBOX
 import com.wutsi.platform.catalog.WutsiCatalogApi
 import com.wutsi.platform.catalog.WutsiCatalogApiBuilder
 import com.wutsi.platform.core.security.feign.FeignAuthorizationRequestInterceptor
-import com.wutsi.platform.core.tracing.feign.FeignAcceptLanguageInterceptor
 import com.wutsi.platform.core.tracing.feign.FeignTracingRequestInterceptor
 import com.wutsi.platform.core.util.feign.Custom5XXErrorDecoder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.core.env.Profiles
+import javax.servlet.http.HttpServletRequest
 
 @Configuration
 class CatalogApiConfiguration(
@@ -23,14 +24,14 @@ class CatalogApiConfiguration(
     private val env: Environment,
 ) {
     @Bean
-    fun catalogApi(): WutsiCatalogApi =
+    fun catalogApi(request: HttpServletRequest): WutsiCatalogApi =
         WutsiCatalogApiBuilder().build(
             env = environment(),
             mapper = mapper,
             interceptors = listOf(
                 tracingRequestInterceptor,
                 authorizationRequestInterceptor,
-                acceptLanguageInterceptor
+                acceptLanguageInterceptor,
             ),
             errorDecoder = Custom5XXErrorDecoder()
         )
